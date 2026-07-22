5220045c4b8b2b2d8bc46aa08ab18a9269937377
Arjun Singh <arjunpride15@gmail.com>
2026-07-22 14:05:55 +0530

    Added some verification system...

--- auth_backend.py
+++ auth_backend.py
@@ -12,6 +12,7 @@ import random
 import tastyerrors
 import datetime, time
 import pyglet.shapes
+from argon2 import PasswordHasher
 
 class SectorEightAuthManager:
     def __init__(self, window):
@@ -19,6 +20,7 @@ class SectorEightAuthManager:
         self.interface = pyglet.graphics.Batch()
         self.data_storage = shelve.open("data\\game_data")
         self.auth_db = shelve.open("data\\auth_db")
+        self.secure_password_dict: dict = self.auth_db.get("password_dict", dict())
         pyglet.options['search_local_libs'] = True
         pyglet.font.add_file('fonts/OpenSans-Regular.ttf')
         self.configObj = conf.Config()
@@ -37,7 +39,18 @@ class SectorEightAuthManager:
         self.register_btn = None
         self.font_list = self.configObj.toml_dict['font']['fontList']
         self.next_btn = None
+        self.state = "welcome"
+        self.username = ""
+        self.argon_hasher = PasswordHasher(
+            time_cost=4,
+            memory_cost=65536,
+            parallelism=5,
+            hash_len=32,
+            salt_len=32
+        )
         
+        
+            
     def play(self, music_file=None):
         
         if music_file == None:
@@ -65,29 +78,120 @@ class SectorEightAuthManager:
         if self.loading_done:
             #print(x, y, button, modifiers)
             if button == mouse.LEFT:
-                if self.log_in_btn.is_clicked(x, y):
-                    self.handle_opt_btn_resize_order(type_="Log In")
-                    self.draw_log_in_page()
-                    
-                    
-                if self.register_btn.is_clicked(x, y):
-                    self.handle_opt_btn_resize_order(type_="Register")
-                    self.draw_register_page()
+                if self.state == "welcome":
+                    if self.log_in_btn.is_clicked(x, y):
+                        self.handle_opt_btn_resize_order(type_="Log In")
+                        self.draw_log_in_page()
+                        
+                        
+                    if self.register_btn.is_clicked(x, y):
+                        self.handle_opt_btn_resize_order(type_="Register")
+                        self.draw_register_page()
                 
-                if self.text_box.text != "":
-                    if self.next_btn.is_clicked(x, y):
-                        self.basic_handle_next()
+                else:
+                    if self.text_box.text != "":
+                        if "usrname_prompt" in self.state:
+                            if self.next_btn.is_clicked(x, y):
+                                self.handle_pin_ui_request()
+                        elif self.state == "log_in_pin_prompt":
+                            if self.next_btn.is_clicked(x, y):
+                                self.verify(self.username, self.text_box.text)
+                        elif self.state == "register_pin_prompt":
+                            if self.next_btn.is_clicked(x, y):
+                                self.create_account(self.username, self.text_box.text)
     
     def draw_log_in_page(self):
         self.text_box.set_visible(True)
         self.next_btn.set_visible(True)
+        self.state = "log_in_usrname_prompt"
 
     def draw_register_page(self):
         self.text_box.set_visible(True)
         self.next_btn.set_visible(True)
+        self.state = "register_usrname_prompt"
     
-    def basic_handle_next(self):
-        print(self.text_box.text)
+    def handle_pin_ui_request(self):
+        #print(self.text_box.text)
+    
+        self.username = self.text_box.text
+        self.welcome_label.text = f"Welcome, {self.username}"
+        
+        self.text_box.masked = True
+    
+        self.text_box.text = ""
+        self.text_box.placeholder = "Enter PIN"
+        self.text_box.update_text_rendering()
+        
+        if self.state == "log_in_usrname_prompt":
+            self.state = "log_in_pin_prompt"
+        elif self.state == "register_usrname_prompt":
+            self.state = "register_pin_prompt"
+    
+    def handle_success(self):
+        self.welcome_label.width = 950
+        self.welcome_label.multiline = True
+        self.welcome_label.color = (255, 255, 255)
+        self.welcome_label.text = \
+        "Success! \n You may now safely close this window. \n Sector Eight Authentication Manager"
+        self.main_card.visible = False
+        self.next_btn.set_visible(False)
+        self.text_box.set_visible(False)
+        
+    
+    def verify(self, username, password):
+        clean_username = username.strip().lower()
+
+        if clean_username not in self.secure_password_dict:
+            self.handle_opt_btn_resize_order(type_="Log In")
+            self.text_box.placeholder_color = (200, 0, 0)
+            self.text_box.masked = False
+            self.text_box.text = ""
+            self.text_box.placeholder = "User doesn't exist!"
+            self.text_box.update_text_rendering()
+            return  # Stop execution!
+
+        stored_hash = self.secure_password_dict[clean_username]
+        
+        try:
+            self.argon_hasher.verify(stored_hash, password)
+            self.text_box.placeholder_color = (110, 115, 125)
+            self.text_box.placeholder = "Enter PIN"
+            self.text_box.update_text_rendering()
+        except Exception:
+            self.text_box.placeholder_color = (200, 0, 0)
+            self.text_box.text = ""
+            self.text_box.placeholder = "Incorrect PIN!"
+            self.text_box.update_text_rendering()
+            return  # Stop execution!
+        else:
+            self.handle_success()
+            
+    
+    def create_account(self, username, password):
+        clean_username = username.strip().lower()
+
+        # 1. Check if user already exists
+        if clean_username in self.secure_password_dict:
+            self.handle_opt_btn_resize_order(type_="Register")
+            self.text_box.placeholder_color = (200, 0, 0)
+            self.text_box.masked = False
+            self.text_box.text = ""
+            self.text_box.placeholder = "User already exists"
+            self.text_box.update_text_rendering()
+            return  # Stop execution!
+
+        # 2. Reset placeholder styling if successful
+        self.text_box.placeholder_color = (110, 115, 125)
+        self.text_box.placeholder = "Enter PIN"
+        self.text_box.update_text_rendering()
+
+        # 3. Hash password and save to dictionary
+        self.secure_password_dict[clean_username] = self.argon_hasher.hash(password)
+        
+        # 4. Write BACK to shelve so changes persist on disk!
+        self.auth_db["password_dict"] = self.secure_password_dict
+        self.auth_db.sync()
+        self.handle_success()
         
     def handle_opt_btn_resize_order(self, type_):
         # First set all Welcome page elements' opacity
@@ -108,7 +212,7 @@ class SectorEightAuthManager:
         self.welcome_label.x = self.main_card.x + 30
         self.welcome_label.y = self.main_card.y + self.main_card.height - 70
         self.welcome_label.text = type_
-        self.welcome_label.font_size = 50
+        self.welcome_label.font_size = 30
         self.welcome_label.font_name = self.font_list
         self.welcome_label.visible = True
         
